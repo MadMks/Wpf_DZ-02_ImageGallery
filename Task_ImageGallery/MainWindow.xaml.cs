3e36349e922fd8b09261a3b97645a7d7b5bb0cc4
T500 <mks59k@gmail.com>
2018-06-16 14:15:37 +0300

    Работает проверка картинок, добавление в listBox. И перелистывание.

--- Task_ImageGallery/MainWindow.xaml.cs
+++ Task_ImageGallery/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using System.Windows.Forms;
 using WinForms = System.Windows.Forms;
 using System.IO;
+using Wpf = System.Windows.Controls;
 
 namespace Task_ImageGallery
 {
@@ -26,7 +27,7 @@ namespace Task_ImageGallery
         /// <summary>
         /// Путь поиска файлов.
         /// </summary>
-        private string path;    // TODO delete
+        //private string path;    // TODO delete
 
         public MainWindow()
         {
@@ -42,16 +43,27 @@ namespace Task_ImageGallery
                 folderBrowserDialog.RootFolder = Environment.SpecialFolder.LocalizedResources;
                 folderBrowserDialog.ShowNewFolderButton = false;
 
-                //DialogResult result = folderBrowserDialog.ShowDialog();
 
                 if (folderBrowserDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
                 {
                     // TODO получаем путь к папке
-                    this.path = folderBrowserDialog.SelectedPath;   // TODO delete
+                    //this.path = folderBrowserDialog.SelectedPath;   // TODO delete
+
+                    // TODO считаю кол-во каринок в папке для прогресБара
 
                     AddToTheListFoundFilesImages(folderBrowserDialog.SelectedPath);     // TODO (string path)
+
+                    if (this.listBox.Items.Count > 0)
+                    {
+                        SettingFirstImageInSlider();
+                    }
                 }
             }
+        }
+
+        private void SettingFirstImageInSlider()
+        {
+            imageSlider.Source = ((this.listBox.Items[0] as Wpf.Button).Content as Image).Source;
         }
 
         private void AddToTheListFoundFilesImages(string selectedPath)
@@ -62,12 +74,51 @@ namespace Task_ImageGallery
 
             foreach (FileInfo fileInfo in directoryInfo.GetFiles())
             {
-                System.Windows.Controls.Button button = new System.Windows.Controls.Button
+                if (IsImageExtensions(fileInfo.Extension) == true)
                 {
-                    Content = "test btn"
-                };
-                this.listBox.Items.Add(button);
+                    this.AddToListBoxImage(fileInfo);
+                }
             }
+        }
+
+        private bool IsImageExtensions(string extension)
+        {
+            if (extension == ".bmp"
+                || extension == ".png"
+                || extension == ".gif"
+                || extension == ".jpg")
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void AddToListBoxImage(FileInfo fileInfo)
+        {
+            Wpf.Button button = new Wpf.Button
+            {
+                Content = "test btn"
+            };
+            button.Click += Button_Click;
+            button.Height = 100;
+            button.Width = this.listBox.ActualWidth;    // TODO перерисовывать!?
+                                                        //button.Margin = new Thickness(10);
+
+
+            Image image = new Image();
+            image.Stretch = Stretch.UniformToFill;
+
+            image.Source = new BitmapImage(new Uri(fileInfo.FullName, UriKind.Absolute));
+            button.Content = image;
+
+            this.listBox.Items.Add(button);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO установка выбранной картинки в слайдер
+            imageSlider.Source = ((sender as Wpf.Button).Content as Image).Source;
         }
     }
 }
